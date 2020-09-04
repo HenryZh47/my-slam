af55b4195b80afca14bfa3c3b23d541498e9d422
Henry Zhang <hzhang0407@gmail.com>
2020-09-04 13:32:37 -0400

    implementation for frontend

---
+++ src/frontend.cpp
@@ -0,0 +1,16 @@
+/*
+ * Frontend class
+ * Author: Henry Zhang
+ */
+
+#include <opencv2/opencv.hpp>
+
+#include "myslam/feature.h"
+#include "myslam/frontend.h"
+#include "myslam/map.h"
+
+namespace myslam {
+
+Frontend::Frontend() {}
+
+}  // namespace myslam
